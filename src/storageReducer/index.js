2ee5471a0ff8959aa9bd67761c740c5e2695e216
Darren Seet <darren@seet.dev>
2020-01-18 22:27:56 -0500

    feat: Use type specific transformations instead of JSON.stringify
    Implement type specific transformation like toString, toISOString or JSON.stringify for setting into storage and Boolean(value), Date(value), Number(value) or JSON.parse fore getting from storage.

--- src/storageReducer/index.js
+++ src/storageReducer/index.js
@@ -1,20 +1,63 @@
 const getItem = (state, action) => {
-  const value = state.getItem(action.key);
-  return JSON.parse(value);
+  const { key, datatype } = action;
+  const value = state.getItem(key);
+  switch (datatype) {
+    case "boolean": {
+      return Boolean(value);
+    }
+    case "date": {
+      return new Date(value);
+    }
+    case "number": {
+      return Number(value);
+    }
+    case "array":
+    case "object": {
+      return JSON.parse(value);
+    }
+  }
+  return value;
 };
 
 const setItem = (state, action) => {
   const { key, value } = action;
-  const type = typeof value;
-  switch (type) {
-    case "function":
-    case "undefined": {
-      throw new Error(
-        "setItem does not accept values of type undefined or function"
-      );
+  let { datatype } = action;
+  if (!("datatype" in action)) {
+    // when data type is not provided use value inspection to determine the datatype.
+    let type = typeof value;
+    if (type === "undefined" || type == "function" || value === null) {
+      throw new Error(`setItem does not support value: ${value}`);
     }
-    default:
+
+    if (type === "object") {
+      if (value instanceof Array) {
+        type = "array";
+      } else if (value instanceof Date) {
+        type = "date";
+      }
+    }
+    datatype = type;
+  }
+
+  switch (datatype) {
+    case "boolean":
+    case "number":
+    case "string": {
+      state.setItem(key, value.toString());
+      break;
+    }
+    case "date": {
+      state.setItem(key, value.toISOString());
+      break;
+    }
+    case "array":
+    case "object": {
       state.setItem(key, JSON.stringify(value));
+      break;
+    }
+    default: {
+      throw new Error(`setItem does support datatype: ${datatype}`);
+    }
   }
 };
 
